42116e96230f57b858ea724fd9370877745b07fb
lauragatius <81159496+lauragatius@users.noreply.github.com>
2021-03-22 14:45:29 +0100

    Add files via upload

--- src/main/java/com/seat/Main.java
+++ src/main/java/com/seat/Main.java
@@ -5,30 +5,37 @@ import static java.lang.System.in;
 import java.util.Scanner;
 
 import com.seat.controller.MowerController;
+import com.seat.controller.PlateauController;
 import com.seat.entity.Mower;
+import com.seat.entity.Plateau;
 
 public class Main {
-	
-	public static void main (String[]args) {
+
+	public static void main(String[] args) {
 		Mower m = new Mower();
-		try{
+		Plateau p = new Plateau();
+		try {
 			MowerController mowerController = new MowerController();
-			int count = 0;
-			while(count<2) {
-				count = count+1;
-				Scanner firstReading = new Scanner(in);
-				Scanner secondReading  = new Scanner(in);
-				
-				String position = firstReading.nextLine();
-				String directions = secondReading.nextLine();
-				
-				MowerController.proccessPosition(position, m);
-				mowerController.proccessDirections(directions, m);
-				
-				mowerController.printPosition(m);
-			}
+			PlateauController plateauController = new PlateauController();
+			Scanner plateauSizeScanner = new Scanner(in);
+			String plateauSize = plateauSizeScanner.nextLine();
+			plateauController.putSize(plateauSize, p);
+
+			Scanner firstReading = new Scanner(in);
+			Scanner secondReading = new Scanner(in);
+
+			String position = firstReading.nextLine();
+			String directions = secondReading.nextLine();
+
+			MowerController.proccessPosition(position, m);
+			mowerController.proccessDirections(directions, m);
 			
-		}catch (Exception e) {
+			plateauController.positionMowerToPlateau(p, m);
+
+			mowerController.printPosition(m);
+			
+
+		} catch (Exception e) {
 			System.out.println("Introduce correct commands.");
 		}
 	}
